661b5a94b20786a3567865684569a35371b2c5fe
Emil-J1 <10407718@ucn.dk>
2022-06-03 10:51:50 +0200

    Header-Update-1

--- assets/script/script.js
+++ assets/script/script.js
@@ -15,7 +15,7 @@ document.addEventListener('click', e => {
   return
 
   let currentDropdown
-  
+
   if (isDropdownButton) {
   currentDropdown = e.target.closest('[data-dropdown]')
   currentDropdown.classList.toggle('active')
@@ -26,7 +26,7 @@ document.addEventListener('click', e => {
     if (dropdown === currentDropdown) 
     return
     
-    dropdown.classList.remove("active")
+    dropdown.classList.remove('active')
   })
 })
 // ---DropdownMenu slut---
